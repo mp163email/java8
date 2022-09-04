2ec6a1e9dc2efb188b92ecebdc77fc53630e13af
mp163email <574474931@qq.com>
2022-09-04 23:42:25 +0800

    测试git的账号密码能否提交

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(2 << 3);
+        System.out.println(Integer.toBinaryString(2));
+        System.out.println(Integer.toBinaryString(2 << 3));
+    }
+}
